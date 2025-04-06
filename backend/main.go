c113d3700b34fc8a1af8c2e098e06c914fe72da1
KrushalGopale <krushalgopale79@gmail.com>
2025-04-06 14:38:30 +0530

    Update

--- backend/main.go
+++ backend/main.go
@@ -1,21 +1,15 @@
 package main
 
 import (
-	"log"
+	
 
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
+	
 	"github.com/krushalgopale/memex/internal/database"
 	"github.com/krushalgopale/memex/internal/routes"
 )
 
 func init() {
-	// load env
-	err := godotenv.Load("../.env")
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
 	// db connection
 	database.ConnDB()
 }
