3fd97540ffd357f0a729b4919d9923a74a16651e
KrushalGopale <krushalgopale79@gmail.com>
2025-04-06 16:35:35 +0530

    Update

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8080';
+const API_BASE_URL = 'https://memex-nf9k.onrender.com';
 
 const handleResponse = async (response) => {
   const data = await response.json();
